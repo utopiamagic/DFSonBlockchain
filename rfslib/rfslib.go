05f4ea7040edd5b11f03e01b871227b8d7c58663
David Ma <qingnian213@hotmail.com>
2018-10-17 15:06:11 -0700

    Added two files

---
+++ rfslib/rfslib.go
@@ -0,0 +1,130 @@
+/*
+
+This package specifies the application's interface to the distributed
+records system (RFS) to be used in project 1 of UBC CS 416 2018W1.
+
+You are not allowed to change this API, but you do have to implement
+it.
+
+*/
+
+package rfslib
+
+import "fmt"
+
+// A Record is the unit of file access (reading/appending) in RFS.
+type Record [512]byte
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// <ERROR DEFINITIONS>
+
+// These type definitions allow the application to explicitly check
+// for the kind of error that occurred. Each API call below lists the
+// errors that it is allowed to raise.
+//
+// Also see:
+// https://blog.golang.org/error-handling-and-go
+// https://blog.golang.org/errors-are-values
+
+// Contains minerAddr
+type DisconnectedError string
+
+func (e DisconnectedError) Error() string {
+	return fmt.Sprintf("RFS: Disconnected from the miner [%s]", string(e))
+}
+
+// Contains filename. The *only* constraint on filenames in RFS is
+// that must be at most 64 bytes long.
+type BadFilenameError string
+
+func (e BadFilenameError) Error() string {
+	return fmt.Sprintf("RFS: Filename [%s] has the wrong length", string(e))
+}
+
+// Contains filename
+type FileDoesNotExistError string
+
+func (e FileDoesNotExistError) Error() string {
+	return fmt.Sprintf("RFS: Cannot open file [%s] in D mode as it does not exist locally", string(e))
+}
+
+// Contains filename
+type FileExistsError string
+
+func (e FileExistsError) Error() string {
+	return fmt.Sprintf("RFS: Cannot create file with filename [%s] as it already exists", string(e))
+}
+
+// Contains filename
+type FileMaxLenReachedError string
+
+func (e FileMaxLenReachedError) Error() string {
+	return fmt.Sprintf("RFS: File [%s] has reached its maximum length", string(e))
+}
+
+// </ERROR DEFINITIONS>
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// Represents a connection to the RFS system.
+type RFS interface {
+	// Creates a new empty RFS file with name fname.
+	//
+	// Can return the following errors:
+	// - DisconnectedError
+	// - FileExistsError
+	// - BadFilenameError
+	CreateFile(fname string) (err error)
+
+	// Returns a slice of strings containing filenames of all the
+	// existing files in RFS.
+	//
+	// Can return the following errors:
+	// - DisconnectedError
+	ListFiles() (fnames []string, err error)
+
+	// Returns the total number of records in a file with filename
+	// fname.
+	//
+	// Can return the following errors:
+	// - DisconnectedError
+	// - FileDoesNotExistError
+	TotalRecs(fname string) (numRecs uint16, err error)
+
+	// Reads a record from file fname at position recordNum into
+	// memory pointed to by record. Returns a non-nil error if the
+	// read was unsuccessful. If a record at this index does not yet
+	// exist, this call must block until the record at this index
+	// exists, and then return the record.
+	//
+	// Can return the following errors:
+	// - DisconnectedError
+	// - FileDoesNotExistError
+	ReadRec(fname string, recordNum uint16, record *Record) (err error)
+
+	// Appends a new record to a file with name fname with the
+	// contents pointed to by record. Returns the position of the
+	// record that was just appended as recordNum. Returns a non-nil
+	// error if the operation was unsuccessful.
+	//
+	// Can return the following errors:
+	// - DisconnectedError
+	// - FileDoesNotExistError
+	// - FileMaxLenReachedError
+	AppendRec(fname string, record *Record) (recordNum uint16, err error)
+}
+
+// The constructor for a new RFS object instance. Takes the miner's
+// IP:port address string as parameter, and the localAddr which is the
+// local IP:port to use to establish the connection to the miner.
+//
+// The returned rfs instance is singleton: an application is expected
+// to interact with just one rfs at a time.
+//
+// This call should only succeed if the connection to the miner
+// succeeds. This call can return the following errors:
+// - Networking errors related to localAddr or minerAddr
+func Initialize(localAddr string, minerAddr string) (rfs RFS, err error) {
+	// TODO
+	// For now return a DisconnectedError
+	return nil, DisconnectedError(minerAddr)
+}
