581366ae8f34728f3323b66fe77386923cd18afb
Justin Awrey <awreyjustin@gmail.com>
2018-10-25 20:10:53 -0700

    add load config from settings

--- miner.go
+++ miner.go
@@ -14,12 +14,13 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"errors"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
+	"os"
 
 	"github.ugrad.cs.ubc.ca/CPSC416-2018W-T1/P1-i8b0b-e8y0b/rfslib"
 	// TODO
@@ -61,23 +62,64 @@ type OperationRecord struct {
 	MinerID       string // An identifier that specifies the miner identifier whose record coins sponsor this operation (op-minerID)
 }
 
-// Miner information is loaded through the configuration file
+// Miner mines blocks.
 type Miner struct {
-	MinerID             string   // The ID of this miner (max 16 characters).
-	PeerMinersAddrs     []string // An array of remote IP:port addresses, one per peer miner that this miner should connect to (using the OutgoingMinersIP below)
-	IncomingMinersAddr  string   // The local IP:port where the miner should expect other miners to connect to it (address it should listen on for connections from miners)
-	OutgoingMinersIP    string   // The local IP that the miner should use to connect to peer miners
-	IncomingClientsAddr string   // The local IP:port where this miner should expect to receive connections from RFS clients (address it should listen on for connections from clients)
+	Settings
+}
 
-	MinedCoinsPerOpBlock   uint8  // The number of record coins mined for an op block
-	MinedCoinsPerNoOpBlock uint8  // The number of record coins mined for a no-op block
-	NumCoinsPerFileCreate  uint8  // The number of record coins charged for creating a file
-	GenOpBlockTimeout      uint8  // Time in milliseconds, the minimum time between op block mining (see diagram above).
-	GenesisBlockHash       string // The genesis (first) block MD5 hash for this blockchain
-	PowPerOpBlock          uint8  // The op block difficulty (proof of work setting: number of zeroes)
-	PowPerNoOpBlock        uint8  // The no-op block difficulty (proof of work setting: number of zeroes)
-	ConfirmsPerFileCreate  uint8  // The number of confirmations for a create file operation (the number of blocks that must follow the block containing a create file operation along longest chain before the CreateFile call can return successfully)
-	ConfirmsPerFileAppend  uint8  // The number of confirmations for an append operation (the number of blocks that must follow the block containing an append operation along longest chain before the AppendRec call can return successfully). Note that this append confirm number will always be set to be larger than the create confirm number (above).
+// Settings contains all miner settings and is loaded through
+// a configuration file.  See:
+// https://www.cs.ubc.ca/~bestchai/teaching/cs416_2018w1/project1/config.json.
+type Settings struct {
+	// The ID of this miner (max 16 characters).
+	MinerID string
+
+	// An array of remote IP:port addresses, one per peer miner that this miner should
+	// connect to (using the OutgoingMinersIP below).
+	PeerMinersAddrs []string
+
+	// The local IP:port where the miner should expect other miners to connect to it
+	// (address it should listen on for connections from miners).
+	IncomingMinersAddr string
+
+	// The local IP that the miner should use to connect to peer miners.
+	OutgoingMinersIP string
+
+	// The local IP:port where this miner should expect to receive connections
+	// from RFS clients (address it should listen on for connections from clients)
+	IncomingClientsAddr string
+
+	// The number of record coins mined for an op block.
+	MinedCoinsPerOpBlock uint8
+
+	// The number of record coins mined for a no-op block.
+	MinedCoinsPerNoOpBlock uint8
+
+	// The number of record coins charged for creating a file.
+	NumCoinsPerFileCreate uint8
+
+	// Time in milliseconds, the minimum time between op block mining.
+	GenOpBlockTimeout uint8
+
+	// The genesis (first) block MD5 hash for this blockchain.
+	GenesisBlockHash string
+
+	// The op block difficulty (proof of work setting: number of zeroes).
+	PowPerOpBlock uint8
+
+	// The no-op block difficulty (proof of work setting: number of zeroes).
+	PowPerNoOpBlock uint8
+
+	// The number of confirmations for a create file operation
+	// (the number of blocks that must follow the block containing a create file operation
+	// along longest chain before the CreateFile call can return successfully).
+	ConfirmsPerFileCreate uint8
+
+	// The number of confirmations for an append operation (the number of blocks
+	// that must follow the block containing an append operation along longest chain
+	// before the AppendRec call can return successfully). Note that this append confirm
+	// number will always be set to be larger than the create confirm number (above).
+	ConfirmsPerFileAppend uint8
 }
 
 // ClientAPI is the set of RPC calls provided to RFS
@@ -131,16 +173,16 @@ func (mapi *MinerAPI) SubmitBlock(blockPacket *ActualBlock, status *bool) error 
 		*status = false
 		return nil
 	}
-	switch t := blockPacket.Block.(type) {
-	default:
-		return errors.New("Invalid Block Type")
-	case OPBlock:
-		fmt.Println("Got OPBlock") // t has type OPBlock
-	case NOPBlock:
-		fmt.Println("Got NOPBlock") // t has type NOPBlock
-	case GenesisBlock:
-		fmt.Println("Got GenesisBlock") // t has type GenesisBlock
-	}
+	// switch t := blockPacket.Block.(type) {
+	// default:
+	// 	return errors.New("Invalid Block Type")
+	// case OPBlock:
+	// 	fmt.Println("Got OPBlock") // t has type OPBlock
+	// case NOPBlock:
+	// 	fmt.Println("Got NOPBlock") // t has type NOPBlock
+	// case GenesisBlock:
+	// 	fmt.Println("Got GenesisBlock") // t has type GenesisBlock
+	// }
 	// quo.Quo = args.A / args.B
 	// quo.Rem = args.A % args.B
 	*status = true
@@ -148,7 +190,7 @@ func (mapi *MinerAPI) SubmitBlock(blockPacket *ActualBlock, status *bool) error 
 }
 
 func (m *Miner) computeNOPBlock() (NOPBlock, error) {
-	minerID := ""
+	// minerID := ""
 	prevHash := ""
 	var nounce uint32
 	nounce = 1
@@ -182,15 +224,50 @@ func broadcastBlock(block ActualBlock, peerMinersAddrs []string) {
 }
 
 func initializeBlockChain(genesisBlockHash string, peerMinersAddrs []string) error {
-	genesisBlock := GenesisBlock{genesisBlockHash}
+	// genesisBlock := GenesisBlock{genesisBlockHash}
 
 	// broadcastBlock(genesisBlock, peerMinersAddrs)
 	return nil
 }
 
-// Main workhorse method.
+// loadJSON loads a settings json file and populates
+// a Settings struct with the data it reads.
+func loadJSON(fn string) (Settings, error) {
+	// Get a file descriptor for the specified file.
+	fi, err := os.Open(fn)
+	if err != nil {
+		return Settings{}, err
+	}
+
+	// Decode the contents of the file into a
+	// Settings struct.
+	var s Settings
+	err = json.NewDecoder(fi).Decode(&s)
+	if err != nil {
+		return Settings{}, err
+	}
+
+	return s, nil
+}
+
+// Main workhorse method.  We are exposing two sets of APIs through RPC;
+// one for other miners, and one for clients.
 func main() {
-	// Register
+
+	// Make sure cmd line input is correct.
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: go run miner.go </path/to/config.json>")
+	}
+
+	// Load up the settings from the specified json file.
+	jsonFn := os.Args[1]
+	settings, err := loadJSON(jsonFn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("settings", settings)
+
+	// Register RPC methods for other miners to call.
 	minerAPI := new(MinerAPI)
 	rpc.Register(minerAPI)
 	rpc.HandleHTTP()
@@ -200,6 +277,7 @@ func main() {
 	}
 	go http.Serve(l, nil)
 
+	// Register RPC methods for clients to call.
 	clientAPI := new(ClientAPI)
 	rpc.Register(clientAPI)
 	rpc.HandleHTTP()
@@ -208,7 +286,4 @@ func main() {
 		log.Fatal("listen error:", e)
 	}
 	go http.Serve(l, nil)
-
-	// Use json.Marshal json.Unmarshal for encoding/decoding to servers
-
 }
